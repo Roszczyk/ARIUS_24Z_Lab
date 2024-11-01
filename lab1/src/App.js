1b4b006dd37539820577796db1ae7c3bdddb4ebc
Roszczyk <mikolaj.roszczyk@onet.pl>
2024-11-01 19:44:28 +0100

    [LAB1] Działa dodawanie tasków

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -1,30 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList';
 import tasksData from './source_data/todo.json';
+import AddTask from './components/AddTask';
+import {v4} from "uuid";
 
 function App(){
   const [tasks, setTasks] = useState(tasksData);
   return(
-    <TaskList 
-      tasks = {tasks}
-      onRemoveTask = {id => {
+    <>
+    <AddTask
+      onNewTask={(title, details, deadline) => {
+        const newTaskList = [
+          ...tasks,
+          {
+            id : v4(),
+            title,
+            details, 
+            deadline,
+            status : "niewykonane",
+            stars : 0
+          }
+        ];
+        setTasks(newTaskList);
+      }} 
+
+    />
+      
+    <TaskList
+      tasks={tasks}
+      onRemoveTask={id => {
         const newTaskList = tasks.filter(task => task.id !== id);
         setTasks(newTaskList);
-      }}
-      onRateTask = {(id, stars) => {
-        const newTaskList = tasks.map(task =>
-          task.id === id ? {...task, stars} : task);
+      } }
+      onRateTask={(id, stars) => {
+        const newTaskList = tasks.map(task => task.id === id ? { ...task, stars } : task);
         setTasks(newTaskList);
-      }}
+      } }
       onChangeStatusTask={id => {
-        const newTaskList = tasks.map(task =>
-          task.id === id
-            ? { ...task, status: task.status === "niewykonane" ? "wykonane" : "niewykonane" }
-            : task
+        const newTaskList = tasks.map(task => task.id === id
+          ? { ...task, status: task.status === "niewykonane" ? "wykonane" : "niewykonane" }
+          : task
         );
         setTasks(newTaskList);
-      }}
-    />
+      } } />
+    </>
   );
 }
 
--- lab1/src/components/AddTask/index.jsx
+++ lab1/src/components/AddTask/index.jsx
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 
-const AddTask = ({ addTask }) => {
+export default function AddTask({onNewTask = f => f}) {
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
   const [deadline, setDeadline] = useState('');
-  const [difficulty, setDifficulty] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTask({
+    onNewTask(
       title,
       details,
-      deadline,
-      done: 'niewykonane',
-      difficulty: parseInt(difficulty, 10)
-    });
+      deadline
+    );
     setTitle('');
     setDetails('');
     setDeadline('');
-    setDifficulty(0);
   };
 
   return (
@@ -26,10 +22,7 @@ const AddTask = ({ addTask }) => {
       <input type="text" placeholder="Tytuł" value={title} onChange={(e) => setTitle(e.target.value)} required />
       <input type="text" placeholder="Szczegóły" value={details} onChange={(e) => setDetails(e.target.value)} required />
       <input type="datetime-local" value={deadline} onChange={(e) => setDeadline(e.target.value)} required />
-      <input type="number" min="0" max="10" value={difficulty} onChange={(e) => setDifficulty(e.target.value)} required />
       <button type="submit">Dodaj zadanie</button>
     </form>
   );
 };
-
-export default AddTask;
