2101253de4cb1729a6557f0b6ea470d1ee0ea7b4
Roszczyk <mikolaj.roszczyk@onet.pl>
2024-11-01 20:00:34 +0100

    [LAB1] Laboratorium ukończone

--- lab1/src/components/AddTask/index.jsx
+++ lab1/src/components/AddTask/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { IoIosAdd } from "react-icons/io";
 
 export default function AddTask({onNewTask = f => f}) {
   const [title, setTitle] = useState('');
@@ -18,11 +19,17 @@ export default function AddTask({onNewTask = f => f}) {
   };
 
   return (
+    <dev>
+    <h2>Dodaj zadanie</h2>
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Tytuł" value={title} onChange={(e) => setTitle(e.target.value)} required />
+      <br/>
       <input type="text" placeholder="Szczegóły" value={details} onChange={(e) => setDetails(e.target.value)} required />
+      <br/>
       <input type="datetime-local" value={deadline} onChange={(e) => setDeadline(e.target.value)} required />
-      <button type="submit">Dodaj zadanie</button>
+      <br/>
+      <button type="submit"><IoIosAdd /><br/>Dodaj zadanie</button>
     </form>
+    </dev>
   );
 };
--- lab1/src/components/Task/index.jsx
+++ lab1/src/components/Task/index.jsx
@@ -4,6 +4,26 @@ import { HiCheck, HiPencil } from "react-icons/hi";
 
 export default function Task ({id, title, details, deadline, status, stars, onRemove = f => f, onRate = f => f, onChangeStatus = f => f})
 {
+  if (status == "niewykonane" && new Date(deadline).getTime() < Date.now()) status = "przeterminowane";
+  if (status === "przeterminowane") return(
+    <section>
+      <h1>{title}</h1>
+      <div><b>Szczegóły:</b> {details}</div>
+      <div><b>Deadline:</b> {deadline}</div>
+      <div><b>Status:</b> {status}</div>
+      <StarRating 
+          selectedStars = {stars}
+          onRate = {stars => onRate(id,stars)}    
+      />
+      <br/>
+      <button onClick = {() => onRemove(id)}>
+        <div><FaTrash/><br/>Usuń zadanie</div>
+      </button>
+      <button onClick = {() => onChangeStatus(id)}>
+        <div><HiCheck/><br/>Wykonane</div>
+      </button>
+    </section>
+  );
   if (status === "wykonane") return(
     <section>
       <h1>{title}</h1>
