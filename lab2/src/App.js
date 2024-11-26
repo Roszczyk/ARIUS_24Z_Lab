7831c38fa479c2e6010b01529498388b4f31be49
Roszczyk <mikolaj.roszczyk@onet.pl>
2024-11-26 11:41:39 +0100

    [LAB2] Pushing finished LAB2

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import { FetchData } from './components/FetchData';
-// import List from "./components/List";
+import List from "./components/List";
+
 
 function App() {
-  return (
-    <div>
-      <FetchData id="MTU3Mzk5Mw"/>
+  const images = FetchData();
+  const renderItem = item => (
+    <div style={{display: "flex"}}>
+      <img src={item.url} alt={item.id} width={100} height={100} />
+      <pre>ID: {item.id}<br />width: {item.width}</pre>
     </div>
   );
+
+  return (
+    <List data={images} renderItem = {renderItem}/>
+  )
 }
 
 export default App;
-
--- lab2/src/components/FetchData/index.jsx
+++ lab2/src/components/FetchData/index.jsx
@@ -3,28 +3,29 @@ import React, { useState, useEffect } from "react";
 const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
 const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
-export function FetchData({ id }) {
-    const [data, setData] = useState(loadJSON(`cat:${id}`));
+export function FetchData() {
+    const [data, setData] = useState(loadJSON("catImages") || []);
 
     useEffect(() => {
-        if(!data) return;
-        const {url, width} = data;
-        saveJSON(`cat:${id}`, {
-            id,
-            url,
-            width
-        });
+        if(data.length > 0) return;
+        fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
+        .then(response => response.json())
+        .then((data) => {
+            data = data.map(({ id, url, width }) => ({ id, url, width }));
+            setData(data);
+            saveJSON("catImages", data);
+        })
+        .catch(console.error);
+    }, []);
+
+    useEffect(() => {
+        if (data.length) {
+            saveJSON("catImages", data);
+        }
     }, [data]);
 
-    useEffect(() => {
-        if(!id) return;
-        if(data && data.id === id) return;
-        fetch(`https://api.thecatapi.com/v1/images/${id}`)
-        .then(response => response.json())
-        .then(response1 => setData(response1))
-        .catch(console.error);
-    }, [id]);
+    // if(data)
+    //     return <pre>{JSON.stringify(data,null,2)}</pre>
 
-    if(data)
-        return <pre>{JSON.stringify(data,null,2)}</pre>
+    if(data) return data;
 }
